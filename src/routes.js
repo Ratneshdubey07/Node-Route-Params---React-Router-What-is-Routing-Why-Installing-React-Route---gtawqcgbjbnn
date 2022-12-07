9e91c9a959452b533dd4615869b886b1a0a03679
Ratneshdubey07 <111305768+Ratneshdubey07@users.noreply.github.com>
2022-12-07 22:42:11 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-
+// router.use()
 
 router.get('/:name/:age', (req, res, next) => {
+
+    res.status(200).json({
+        message: `Hello ${req.params.name}, you are ${req.params.age} years old`
+    })
 })
 module.exports = router;
